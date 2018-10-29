16d9ad959baaa6041fa72db6f7a8880d3c76efc4
G Singh <gssrann@gmail.com>
2018-10-29 20:36:40 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+let app = express();
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    fs.appendFile('server.log', `${now} : ${req.method}, ${req.url} \n`, (err) => {
+        console.log('Unable to append to server log');
+    });
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send({name: 'test'});
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on 3000')
+});
